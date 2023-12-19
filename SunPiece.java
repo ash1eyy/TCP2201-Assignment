52c3cb753ec586c9fee21328df1007bd06294adf
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2023-12-20 01:49:31 +0800

    Delete SunPiece.java

--- SunPiece.java
+++
@@ -1,8 +0,0 @@
-public class SunPiece extends Piece{
-    public SunPiece(int pieceX, int pieceY, String pieceColor)
-    {super(pieceX, pieceY, pieceColor);}
-    @Override
-    public boolean isValidMove(int newX, int newY)
-    {int deltaX 
-    
-}
