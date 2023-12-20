02392fdd1fa53440f35b58a05487028dfbf1397f
Ashley Sim <53300328+ash1eyy@users.noreply.github.com>
2023-12-20 18:45:46 +0800

    Update SunPiece.java

--- Talabia/SunPiece.java
+++ Talabia/SunPiece.java
@@ -9,36 +9,36 @@ public class SunPiece extends Piece {
 
     @Override
     public boolean isValidMove(Board board, int newX, int newY) {
-        switch (getDir()) {
-            case "Up":
-                if (newX == this.getX() - 1) {
-                    return true;
-                }
+        if (Math.abs(newY - this.getY()) > 1 ||
+            Math.abs(newX - this.getX()) > 1) //must be within 1 move
+            return false;
 
-            case "Down":
-                if (newX == this.getX() + 1) {
-                    return true;
-                }
-
-            case "Left":
-                if (newY == this.getY() - 1) {
-                    return true;
-                }
-
-            case "Right":
-                if (newY == this.getY() + 1) {
-                    return true;
-                }
-
-            case "DiagUpLeft":
-
-            case "DiagDownLeft":
-
-            case "DiagUpRight":
-
-            case "DiagDownRight":
+        if (board.getPiece(newX, newY) != null) {
+            if (board.getPiece(newX, newY).getColour() == this.getColour()) //must be a diff colour
+                return false;
         }
-        return false;
+
+        //checks the direction that the player wants to move the piece in (IMAGINE TICTACTOE GRID)
+        if (this.getY() != newY) { //either up/down/upleft/upright/downleft/downright
+            if (newY > this.getY())// down/downleft/downright
+                this.setDir("down");
+            else if (newY < this.getY()) // up/upleft/upright
+                this.setDir("up");
+
+            if (newX < this.getX())
+                this.setDir(this.getDir() + "left");
+            else if (newX > this.getX())
+                this.setDir(this.getDir() + "right");
+        }
+        else { //either left or right
+            if (newX > this.getX()) //right
+                this.setDir("right");
+            else if (newX < this.getX()) //left
+                this.setDir("left");
+        }
+
+        System.out.println("Direction: " + this.getDir());
+        return true;
     }
 
     @Override
