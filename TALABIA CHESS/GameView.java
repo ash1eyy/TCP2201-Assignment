299267f2660389ddfa4a12486f56cd9b385b21c7
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-11 15:24:11 +0800

    Update GameView.java

--- TALABIA CHESS/GameView.java
+++ TALABIA CHESS/GameView.java
@@ -2,71 +2,83 @@ import javax.swing.*;//imports
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
 
 public class GameView extends JFrame { // zaf and thash doing this, hello
+
+    private GameController controller;
 
     public GameView() {
         super("Talabia Chess");
         JPanel panel = new JPanel();
 
         // default size for board
-        setSize(500,500);
+        setSize(800, 800);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // make the window resizeable
         setResizable(true);
 
-        addGameBoard(); //adds the gameboard into GUI
+        addGameBoard(); // adds the gameboard into GUI
 
-
-        addControlPanel();//adds the Control Into GUI
+        addControlPanel();// adds the Control Into GUI
 
         setVisible(true);// make frame visible
     }
 
     // add components
     public void addGameBoard() {
+
         JPanel boardPanel = new JPanel();
         boardPanel.add(new JLabel("Game Board Here")); // this replace with an actual game board
-        add(boardPanel, BorderLayout.CENTER); //add to the center of the frame
+        add(boardPanel, BorderLayout.CENTER); // add to the center of the frame
+
+        GridLayout grid = new GridLayout(controller.getGameModel().getX(), controller.getGameModel().getY());
+        boardPanel.setLayout(grid);
+
+        ArrayList<ArrayList<Piece>> map = controller.getGameModel().getMap();
+        for (int i = 0; i < controller.getGameModel().getY(); i++) {
+            for (int j = 0; j < controller.getGameModel().getX(); j++) {
+
+                Piece piece = map.get(i).get(j);
+
+            }
+        }
     }
 
     public void addControlPanel() {
         JPanel controlPanel = new JPanel();
         add(controlPanel);
 
-
-        //the start button for the game
+        // the start button for the game
         JButton startButton = new JButton("Start");
         controlPanel.add(startButton);
-        startButton.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO
-			}
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO
+            }
         });
-        
 
-        //the save button for the game 
+        // the save button for the game
         JButton saveButton = new JButton("Save");
         controlPanel.add(saveButton);
-        saveButton.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//prompts the user to input a filename
-			}
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // prompts the user to input a filename
+            }
         });
 
-
-        //the exit button for the game 
+        // the exit button for the game
         JButton exitButton = new JButton("Exit");
         controlPanel.add(exitButton);
-        exitButton.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Game.exit();
-			}
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Game.exit();
+            }
         });
     }
 }
