2f3caa412179d17fccfc3c04868c63c7ff06a998
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-11 22:13:55 +0800

    Update PlusPiece.java

--- TALABIA CHESS/PlusPiece.java
+++ TALABIA CHESS/PlusPiece.java
@@ -1,90 +1,98 @@
-public abstract class Piece {
-    private int pieceX;
-    private int pieceY;
-    private String pieceColour;
-    private String direction;
-
-    Piece() {
+public class PlusPiece extends Piece {
+    PlusPiece() {
+        super();
     }
 
-    Piece(int pieceX, int pieceY, String pieceColour, String direction) { // zaf did this
-        this.pieceX = pieceX;
-        this.pieceY = pieceY;
-        this.pieceColour = pieceColour;
-        this.direction = direction;
+    PlusPiece(int pieceX, int pieceY, String pieceColour, String direction) {
+        super(pieceX, pieceY, pieceColour, direction);
     }
 
-    public int getX() {
-        return pieceX;
-    }
-
-    public int getY() {
-        return pieceY;
-    }
-
-    public String getColour() {
-        return pieceColour;
-    }
-
-    public String getDir() {
-        return direction;
-    }
-
-    public void setX(int pieceX) {
-        this.pieceX = pieceX;
-    }
-
-    public void setY(int pieceY) {
-        this.pieceY = pieceY;
-    }
-
-    public void setColour(String colour) {
-        this.pieceColour = colour;
-    }
-
-    public void setDir(String direction) {
-        this.direction = direction;
-    }
-
-    public abstract String getPiece();
-    
-    // by ashley :-)
-    public boolean move(Board board, int newX, int newY, String userColour) {
-        if (isValidMove(board, newX, newY) && userColour == board.getPiece(getX(), getY()).getColour()) {
-            board.setPiece(getX(), getY(), null);
-            board.setPiece(newX, newY, this);
-            System.out.println("\nSuccessfully moved piece.\n");
-            return true;
-        } else {
-            System.out.println("\nNot a valid move.\n");
-            return false;
+    //by ashley :D
+    @Override
+    public boolean isValidMove(Board board, int newX, int newY) {
+        //checks the direction that the player wants to move the piece in
+        if (this.getX() == newX) { //either up or down
+            if (newY > this.getY()) // down
+                this.setDir("down");
+            else //up
+                this.setDir("up");
         }
-    };
-
-    public abstract boolean isValidMove(Board board, int newX, int newY);
-
-    // this is a capture method that declares targetPiece variable from the pieces
-    // on the board
-    public void capture(Board board, int pieceX, int pieceY) { // zaf did this
-        Piece targetPiece = board.getPiece(pieceX, pieceY);
-        if (this.pieceColour != targetPiece.getColour()) { // can capture
-            board.setPiece(pieceX, pieceY, this); // "this" is the current piece that you are holding
-            board.setPiece(this.getX(), this.getY(), null);
-            System.out.println("You captured the piece!");
-        } else { // cannot capture
-                 // display error
-            System.out.println("You cannot capture your own piece"); // to replace with throw exception
+        else { //either left or right
+            if (newX > this.getX()) //right
+                this.setDir("right");
+            else //left
+                this.setDir("left");
         }
+
+        Piece pieceInFront;
+
+        //checks all of the spaces in the direction that the piece wants to move
+        //EXCEPT for the destination space
+        //if a piece is detected, move is invalid
+        switch (this.getDir()) {
+            case "left":
+                for (int i = 1; i < Math.abs(newX - this.getX()) + 1; i++) { //we use math abs so there is no negative numbers
+                    pieceInFront = board.getPiece(this.getX() - i, this.getY());
+
+                    if (pieceInFront != null) {
+                        return false;
+                    }
+                    continue;
+                }
+                break;
+
+            case "right":
+                for (int i = 1; i < Math.abs(newX - this.getX()) + 1; i++) {
+                    pieceInFront = board.getPiece(this.getX() + i, this.getY());
+
+                    if (pieceInFront != null) {
+                        return false;
+                    }
+                    continue;
+                }
+                break;
+
+            case "up":
+                for (int i = 1; i < Math.abs(newY - this.getY()) + 1; i++) {
+                    pieceInFront = board.getPiece(this.getX(), this.getY() - i);
+
+                    if (pieceInFront != null) {
+                        return false;
+                    }
+                    continue;
+                }
+                break;
+
+            case "down":
+                for (int i = 1; i < Math.abs(newY - this.getY()) + 1; i++) {
+                    pieceInFront = board.getPiece(this.getX(), this.getY() + i);
+
+                    if (pieceInFront != null) {
+                        return false;
+                    }
+                    continue;
+                }
+                break;
+        }
+        
+        //if the piece at the plus piece's destination does not share the same colour,
+        //it is a valid move because it can capture the piece, otherwise it is invalid
+        if (board.getPiece(newX, newY) != null) {
+            if (board.getPiece(newX, newY).getColour() != this.getColour())
+                return true;
+            else
+                return false;
+        }
+        return true;
     }
 
-    public boolean check(Board board, int pieceX, int pieceY) {
+    @Override
+    public String toString() {
+        return "+";
+    }
 
-        Piece pieceChecked = board.getPiece(pieceX, pieceY);
-
-        getSunPiece()
-        return false;
-    } // checkmate method
-
-
-    //also update the move() method and have it check if there is a check 
+    @Override
+    public String getPiece(){
+        return "plus";
+    }
 }
