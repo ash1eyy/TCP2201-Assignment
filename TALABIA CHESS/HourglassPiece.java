615b9bfbc0ac97d3496e75bbaf7168183ab5258d
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-17 01:39:46 +0800

    Update HourglassPiece.java

--- TALABIA CHESS/HourglassPiece.java
+++ TALABIA CHESS/HourglassPiece.java
@@ -14,8 +14,6 @@ public class HourglassPiece extends Piece {
             if (board.getPiece(newX, newY).getColour() == this.getColour())         // must be a diff colour
                 return false;
         }
-
-        //we use math abs so there is no negative numbers
         
         if (Math.abs(newY - this.getY()) == 2 && Math.abs(newX - this.getX()) == 1) // must be within the range of 3x2
             return true;                                                            // Vertical 3x2
@@ -27,12 +25,12 @@ public class HourglassPiece extends Piece {
     }
 
     @Override
-    public String toString() {
-        return "H";
+    public String getPiece() {
+        return "hourglass";
     }
 
     @Override
-    public String getPiece() {
-        return "HourGlass";
+    public String toString() {
+        return "H";
     }
 }
