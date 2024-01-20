e0e756d0b59f54dc62e100ffa1724b05868d8ab9
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-20 22:29:06 +0800

    Update PointPiece.java

--- TALABIA CHESS/PointPiece.java
+++ TALABIA CHESS/PointPiece.java
@@ -1,5 +1,4 @@
 public class PointPiece extends Piece {
-
     PointPiece() {
         super();
     }
@@ -11,11 +10,14 @@ public class PointPiece extends Piece {
     // by ashley ^o^
     @Override
     public boolean isValidMove(Board board, int newX, int newY) {
+        //System.out.println("Num of moves: " + Math.abs(newY - this.getY()));
         if (Math.abs(newY - this.getY()) > 2) // must be within 1-2 moves
             return false;
 
+        if (newX != this.getX())
+            return false;
+
         // this is to detect same colour pieces in pathway of current piece so they dont overlap
-        
         if (board.getPiece(newX, newY) != null) {
             if (board.getPiece(newX, newY).getColour() == this.getColour()) // must be a diff colour
                 return false;
@@ -32,8 +34,6 @@ public class PointPiece extends Piece {
 
                 if (pieceInFront != null)
                     return false;
-
-                continue;
             }
         } 
         else if (this.getDir() == "down") {
@@ -42,16 +42,15 @@ public class PointPiece extends Piece {
 
                 if (pieceInFront != null)
                     return false;
-
-                continue;
             }
         }
+
         return true;
     }
 
     @Override
-    public String getPiece() {
-        return "point";
+    public String getPieceName() {
+        return "point" + this.getDir();
     }
 
     @Override
