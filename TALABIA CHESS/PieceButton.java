2c090ef999ac85d78eec3e46a7eab710289f24e6
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-20 22:28:20 +0800

    Create PieceButton.java

---
+++ TALABIA CHESS/PieceButton.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class PieceButton extends JButton {
+    private Piece piece;
+    private boolean selected = false;
+
+    PieceButton() {}
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public boolean getSelected() {
+        return selected;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
