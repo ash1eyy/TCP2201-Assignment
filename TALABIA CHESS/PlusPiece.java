50034d4f0486afde4042f725f69c6fdd6c44e07e
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-21 00:08:42 +0800

    Update PlusPiece.java

--- TALABIA CHESS/PlusPiece.java
+++ TALABIA CHESS/PlusPiece.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class PlusPiece extends Piece {
     PlusPiece() {
         super();
@@ -7,87 +9,128 @@ public class PlusPiece extends Piece {
         super(pieceX, pieceY, pieceColour, direction);
     }
 
-    //by ashley :D
     @Override
-    public boolean isValidMove(Board board, int newX, int newY) {
-        //checks the direction that the player wants to move the piece in
-        if (this.getX() == newX) { //either up or down
-            if (newY > this.getY()) // down
-                this.setDir("down");
-            else //up
-                this.setDir("up");
-        }
-        else { //either left or right
-            if (newX > this.getX()) //right
-                this.setDir("right");
-            else //left
-                this.setDir("left");
-        }
+    public ArrayList<ArrayList<Integer>> getValidMoves(Board board) {
+        ArrayList<ArrayList<Integer>> validMoves = new ArrayList<ArrayList<Integer>>();
 
-        Piece pieceInFront;
+        int checkX, checkY;
+        int[][] nextCoords = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        //checks all of the spaces in the direction that the piece wants to move
-        //EXCEPT for the destination space
-        //if a piece is detected, move is invalid
-        switch (this.getDir()) {
-            case "left":
-                for (int i = 1; i < Math.abs(newX - this.getX()) + 1; i++) { //newX+= is go right, newX-= is go left
-                    pieceInFront = board.getPiece(this.getX() - i, this.getY());
+        for (int[] next : nextCoords) {
+            checkX = this.getX() + next[0];
+            checkY = this.getY() + next[1];
 
-                    if (pieceInFront != null) { //this is to check if there is a piece in front
-                        return false;
-                    }
+            while (checkX < board.getX() &&
+                checkY < board.getY() &&
+                checkX >= 0 && checkY >= 0) {
+                
+                //if given space is empty, continue
+                if (board.isEmpty(checkX, checkY)) {
+                    validMoves.add(new ArrayList<Integer>());
+                    validMoves.get(validMoves.size() - 1).add(checkX);
+                    validMoves.get(validMoves.size() - 1).add(checkY);
+
+                    checkX += next[0];
+                    checkY += next[1];
                     continue;
                 }
-                break;
-
-            case "right":
-                for (int i = 1; i < Math.abs(newX - this.getX()) + 1; i++) {
-                    pieceInFront = board.getPiece(this.getX() + i, this.getY());
-
-                    if (pieceInFront != null) {
-                        return false;
+                else {
+                    //if current piece and piece at space are diff colours, continue
+                    if (this.getColour() != board.getPiece(checkX, checkY).getColour()) {
+                        validMoves.add(new ArrayList<Integer>());
+                        validMoves.get(validMoves.size() - 1).add(checkX);
+                        validMoves.get(validMoves.size() - 1).add(checkY);
+                        break;
                     }
-                    continue;
                 }
                 break;
-
-            case "up":
-                for (int i = 1; i < Math.abs(newY - this.getY()) + 1; i++) {//newY+= is go down, newY-=
-                    pieceInFront = board.getPiece(this.getX(), this.getY() - i);
-
-                    if (pieceInFront != null) {
-                        return false;
-                    }
-                    continue;
-                }
-                break;
-
-            case "down":
-                for (int i = 1; i < Math.abs(newY - this.getY()) + 1; i++) {
-                    pieceInFront = board.getPiece(this.getX(), this.getY() + i);
-
-                    if (pieceInFront != null) {
-                        return false;
-                    }
-                    continue;
-                }
-                break;
+            }
         }
-        
-        //if the piece at the plus piece's destination does not share the same colour,
-        //it is a valid move because it can capture the piece, otherwise it is invalid
-        if (board.getPiece(newX, newY) != null) {
-            if (board.getPiece(newX, newY).getColour() != this.getColour())
-                return true;
-            else
-                return false;
-        }
-        return true;
+
+        return validMoves;
     }
 
+    //by ashley :D
+    // @Override
+    // public boolean isValidMove(Board board, int newX, int newY) {
+    //     //checks the direction that the player wants to move the piece in
+    //     if (this.getX() == newX) { //either up or down
+    //         if (newY > this.getY()) // down
+    //             this.setDir("down");
+    //         else //up
+    //             this.setDir("up");
+    //     }
+    //     else { //either left or right
+    //         if (newX > this.getX()) //right
+    //             this.setDir("right");
+    //         else //left
+    //             this.setDir("left");
+    //     }
+
+    //     Piece pieceInFront;
+
+    //     //checks all of the spaces in the direction that the piece wants to move
+    //     //EXCEPT for the destination space
+    //     //if a piece is detected, move is invalid
+    //     switch (this.getDir()) {
+    //         case "left":
+    //             for (int i = 1; i < Math.abs(newX - this.getX()) + 1; i++) { //newX+= is go right, newX-= is go left
+    //                 pieceInFront = board.getPiece(this.getX() - i, this.getY());
+
+    //                 if (pieceInFront != null) { //this is to check if there is a piece in front
+    //                     return false;
+    //                 }
+    //                 continue;
+    //             }
+    //             break;
+
+    //         case "right":
+    //             for (int i = 1; i < Math.abs(newX - this.getX()) + 1; i++) {
+    //                 pieceInFront = board.getPiece(this.getX() + i, this.getY());
+
+    //                 if (pieceInFront != null) {
+    //                     return false;
+    //                 }
+    //                 continue;
+    //             }
+    //             break;
+
+    //         case "up":
+    //             for (int i = 1; i < Math.abs(newY - this.getY()) + 1; i++) {//newY+= is go down, newY-=
+    //                 pieceInFront = board.getPiece(this.getX(), this.getY() - i);
+
+    //                 if (pieceInFront != null) {
+    //                     return false;
+    //                 }
+    //                 continue;
+    //             }
+    //             break;
+
+    //         case "down":
+    //             for (int i = 1; i < Math.abs(newY - this.getY()) + 1; i++) {
+    //                 pieceInFront = board.getPiece(this.getX(), this.getY() + i);
+
+    //                 if (pieceInFront != null) {
+    //                     return false;
+    //                 }
+    //                 continue;
+    //             }
+    //             break;
+    //     }
+        
+    //     //if the piece at the plus piece's destination does not share the same colour,
+    //     //it is a valid move because it can capture the piece, otherwise it is invalid
+    //     if (board.getPiece(newX, newY) != null) {
+    //         if (board.getPiece(newX, newY).getColour() != this.getColour())
+    //             return true;
+    //         else
+    //             return false;
+    //     }
+    //     return true;
+    // }
+
     @Override
-    public String getPiece() {
+    public String getPieceName() {
         return "plus";
     }
 
