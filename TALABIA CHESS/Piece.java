34c2c987d0f33c18af21da974ca20e5f5bf7d150
MUHAMMAD ZAFRAN BIN MOHD ANUAR <123632680+1211101321@users.noreply.github.com>
2024-01-24 14:59:41 +0800

    Update Piece.java comments

--- TALABIA CHESS/Piece.java
+++ TALABIA CHESS/Piece.java
@@ -1,22 +1,47 @@
 import java.util.*;
 
 public abstract class Piece{ 
-    //data field
+
+    /* 
+    
+    Data Field
+
+    pieceX and pieceY : X and Y coordinate of piece declared here
+
+    piece Colour : Colour of Piece
+
+    direction: Direction of Piece
+    
+    
+    */
+
     private int pieceX;
     private int pieceY;
     private String pieceColour;
     private String direction;
 
+    /* 
+    
+    Constructor
+    
+    */
+
     Piece() {}
 
-    Piece(int pieceX, int pieceY, String pieceColour, String direction) {  //zaf did this
+    Piece(int pieceX, int pieceY, String pieceColour, String direction) {  //zafran did this
         this.pieceX = pieceX;
         this.pieceY = pieceY;
         this.pieceColour = pieceColour;
         this.direction = direction;
     }
+    
+    /* 
+    
+    Getters and Setters methods to get and set the private varaiables
 
-    public int getX() { //getters setters
+    */
+
+    public int getX() { 
         return pieceX;
     }
 
@@ -24,11 +49,11 @@ public abstract class Piece{
         return pieceY;
     }
 
-    public String getColour() {
+    public String getColour() { 
         return pieceColour;
     }
 
-    public String getDir() {
+    public String getDir() { 
         return direction;
     }
 
@@ -48,8 +73,14 @@ public abstract class Piece{
         this.direction = direction;
     }
 
+    /* 
+    
+    This is a method that gets the name of the Piece
+
+    */
     public abstract String getPieceName();
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //by ashley :-)
     // public boolean move(Board board, int newX, int newY, String userColour) {
     //     if (isValidMove(board, newX, newY) && userColour == board.getPiece(getX(), getY()).getColour()) { //valid move method that will be used for each piece
@@ -63,11 +94,54 @@ public abstract class Piece{
     //         return false;
     //     }
     // };
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public abstract ArrayList<ArrayList<Integer>> getValidMoves(Board board);
+
+    /* 
+    
+    Valid moves method that is made of 2d ArrayList also 
+
+    Parameters used 
+    
+    board : Board 
+    player : the player is A String variable
+    
+    */
+
+    public abstract ArrayList<ArrayList<Integer>> getValidMoves(Board board, String player);
+
+    /* 
+    
+    The add valid moves method has the following parameters
+
+    validMoves : 2d ArrayLists of Integers 
+    validX : valid X coordinate
+    validY : valid Y coordinate
+
+    */
 
 
-    // this is a capture method that declares targetPiece variable from the pieces on the board
+    public void addValidMove(ArrayList<ArrayList<Integer>> validMoves, int validX, int validY) {
+        validMoves.add(new ArrayList<Integer>());
+        validMoves.get(validMoves.size() - 1).add(validX);
+        validMoves.get(validMoves.size() - 1).add(validY);
+        /* 
+    
+        What does this do ah
+
+        */
+    }
+
+    /* 
+    
+    This is a capture method that declares targetPiece variable from the pieces on the board
+
+    board : Board 
+    pieceX : X coordinate
+    PieceY : Y coordinate
+
+    */
+
     public void capture(Board board, int pieceX, int pieceY) { //by zafran 
         Piece targetPiece = board.getPiece(pieceX, pieceY);   
         
